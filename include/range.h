5a9537fe49226fc580eeb152286bb56443787693
jazminbelmonte <jazminbelmonte@gmail.coim>
2018-09-11 20:53:18 -0600

    rough layout

--- include/range.h
+++ include/range.h
@@ -1,1 +1,32 @@
 //TODO: Class Range defined
+#ifndef RANGE_H
+#define RANGE_H
+#include <vector>
+
+template <class R>
+class Range {
+    public:
+        std::vector<std::int> values(){ return data; }
+        int      length() const { return lengthOf; }
+        double      sum() const
+        double      average() const;
+        double      max() const;
+        double      min() const;
+        double      values() const;
+        friend	    ostream& operator<<(ostream& out, Range& r);
+    private:
+        //implementing vector of data
+        std::vector<std::string> data;
+        std::int startNum;
+        std::int endNum;
+        std::int step;
+};
+
+ostream& operator<<(ostream& out, lentgh& len)
+{
+    out << values() << endl;
+
+    return out;
+}
+
+#endif //RANGE_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,20 @@
 //TODO: main method goes here
+
+#include <iostream>
+#include "range.h"
+using namespace std;
+
+int main()
+{
+    stack<int>  s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    cout << s.pop() << endl;
+    cout << s.pop() << endl;
+    cout << s.pop() << endl;
+
+    return 0;
+}
